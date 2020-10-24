634c94f86e3fe582496f3bcbae6613d2d4821c84
Marc-Philippe Huget <mphuget@gmail.com>
2020-10-24 21:24:56 +0200

    Add to Firestore

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const fs = require('firebase-admin');
 
-const serviceAccount = require('./Certificates/gentle-voltage-293516-3b9e09fd9ae6.json');
+const serviceAccount = require('./Certificates/gentle-voltage-293516-bdc58c06b2ca.json');
 
 fs.initializeApp({
  credential: fs.credential.cert(serviceAccount)
 });
 
 const db = fs.firestore(); 
+
+const todoDb = db.collection('todos');
+
+function addTodo(id, title, description) {
+
+    const idTodo = todoDb.doc(id); 
+
+    idTodo.set({
+        title: title,
+        description: description
+    });
+
+}
+
+addTodo("shopping", "Shopping", "Take the shopping list");
+
